01e2f0122e46f2c759423c7e2ce56bd277fd833c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-09-02 06:09:59 -0300

    fix(frontend-analysis): update html and js

--- frontend/assets/js/analysis.js
+++ frontend/assets/js/analysis.js
@@ -1,2 +1,33 @@
-document.getElementById("plot-rating-gross").src = "http://127.0.0.1:8000/plots/imbd_vs_gross";
-document.getElementById("plot-genres").src = "http://127.0.0.1:8000/plots/genres";
+const plots = [
+    { url: "http://127.0.0.1:8000/plots/correlations/download", name: "heatmap_correlations" },
+    { url: "http://127.0.0.1:8000/plots/imbd_vs_gross/download", name: "imdb_vs_gross" },
+    { url: "http://127.0.0.1:8000/plots/genres/download", name: "top10_genres" },
+    { url: "http://127.0.0.1:8000/plots/histogram/IMDB_Rating/download", name: "histogram_IMDB_Rating" },
+];
+
+const container = document.getElementById("plots-container");
+
+plots.forEach(plot => {
+    fetch(plot.url)
+        .then(response => response.blob())
+        .then(blob => {
+            const imgURL = URL.createObjectURL(blob);
+
+            const img = document.createElement("img");
+            img.src = imgURL;
+            img.alt = plot.name;
+            img.style.maxWidth = "600px";
+            img.style.marginBottom = "20px";
+
+            const link = document.createElement("a");
+            link.href = imgURL;
+            link.download = plot.name + ".png";
+            link.textContent = "Download " + plot.name;
+            link.style.display = "block";
+            link.style.marginBottom = "40px";
+
+            container.appendChild(img);
+            container.appendChild(link);
+        })
+        .catch(err => console.error("Erro ao baixar plot:", err));
+});
